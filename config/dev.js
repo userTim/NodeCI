77742a8176f326d7e70880b1f6caf8e36c82136f
userTim <usertim1205@gmail.com>
2020-12-03 17:55:22 +0900

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://usertim:2718281@node-rest-shop-shard-00-00.xdpwg.mongodb.net:27017,node-rest-shop-shard-00-01.xdpwg.mongodb.net:27017,node-rest-shop-shard-00-02.xdpwg.mongodb.net:27017/advancedNode?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true&w=majority',  
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
 };
---
+++ tests/setup.js
@@ -0,0 +1,10 @@
+jest.setTimeout(30000)
+
+require('../models/User')
+require('../models/Blog')
+
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.mongoURI, { useMongoClient: true })
